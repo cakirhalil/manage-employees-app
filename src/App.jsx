20e83c3f5681764d6a8a1469610e6f00bdcb7fca
cakirhalil <halilcakir343@gmail.com>
2025-02-28 19:08:42 +0300

    Created app

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import AddEmployeeModal from "./components/AddEmployeeModal";
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+
+    const [employees, setEmployees] = useState([
+        {
+            id: 1,
+            name: "David Hardy",
+            email: "thomashardy@mail.com",
+            address: "89 Chiaroscuro Rd, Portland, USA",
+            phone: "(171) 555-2222"
+        }
+    ]);
+
+    function addEmployee(newEmployee) {
+        setEmployees(prevEmployees => [
+            ...prevEmployees,
+            {
+                ...newEmployee,
+                id: Math.max(...prevEmployees.map(emp => emp.id), 0) + 1
+            }
+        ])
+    }
+
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+    return (
+        <div className="container">
+            <div className="table-wrapper">
+                <Header onOpenAddModal={() => setIsAddModalOpen(true)} />
+                <EmployeeList employees={employees} />
+                <AddEmployeeModal isOpen={isAddModalOpen} onCloseAddModal={() => setIsAddModalOpen(false)} onAddEmployee={addEmployee} />
+            </div>
+        </div>
+    )
+
+}
+
+export default App;
